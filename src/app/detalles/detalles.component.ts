e84a211e4d05aef06d0f457c3700a8fa5b59cc4d
Xv120926 <xvyadira1209@gmail.com>
2026-07-29 01:16:31 -0600

    Cambios imagenes

--- src/app/detalles/detalles.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-detalles',
-  imports: [],
-  templateUrl: './detalles.component.html',
-  styleUrl: './detalles.component.css',
-})
-export class DetallesComponent {}
